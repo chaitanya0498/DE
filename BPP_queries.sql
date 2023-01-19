5620752ed62214d5d6ec77ca9326e329cc218eb5
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2023-01-19 17:59:20 +0100

    Update BPP_queries.sql

--- BPP_queries.sql
+++ BPP_queries.sql
@@ -9,3 +9,11 @@ select * from BPP_Cliente;
 #ALTER TABLE BPP_Cliente
 #ADD CONSTRAINT CK_NumeroTelefono
 #CHECK (DATALENGTH(NumeroTelefono) = 10);
+
+
+#INSERT INTO BPP_Cliente(CodiceCliente,TipologiaCliente,Nome,Cognome,NumeroTelefono,Indirizzo,RagioneSociale,NomeReferente,CognomeReferente)
+#VALUES ( '001967865-8','Privato','Letizia','Palumbo','0922817865','Via Dante','','','')
+
+#ALTER TABLE BPP_Cliente
+#ADD CONSTRAINT CK_Privato
+#CHECK (TipologiaCliente='Privato' AND (RagioneSociale='N/A' OR RagioneSociale = NULL) AND NomeReferente = NULL AND CognomeReferente = NULL);
