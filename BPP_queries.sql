5282b23f9f9cef43351750a782e07fe267fdaef0
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2023-01-20 09:44:46 +0100

    Update BPP_queries.sql

--- BPP_queries.sql
+++ BPP_queries.sql
@@ -1,6 +1,6 @@
-#UPDATE BPP_Cliente
-#SET [RagioneSociale] = 'N/A'
-#WHERE [TipologiaCliente] = 'Privato';
+--UPDATE BPP_Cliente
+--SET [RagioneSociale] = 'N/A'
+--WHERE [TipologiaCliente] = 'Privato';
 
 
 select * from BPP_Cliente;
@@ -13,6 +13,12 @@ select * from BPP_Cliente;
 
 #INSERT INTO BPP_Cliente(CodiceCliente,TipologiaCliente,Nome,Cognome,NumeroTelefono,Indirizzo,RagioneSociale,NomeReferente,CognomeReferente)
 #VALUES ( '001967865-8','Privato','Letizia','Palumbo','0922817865','Via Dante','','','')
+
+
+--UPDATE BPP_Cliente
+--SET NomeReferente = NULL, CognomeReferente = NULL 
+--WHERE TipologiaCliente = 'Privato'
+
 
 #ALTER TABLE BPP_Cliente
 #ADD CONSTRAINT CK_Privato
