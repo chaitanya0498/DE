44b4973ca71b180c383788acb8de88da27cb4ffb
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2023-01-20 14:31:21 +0100

    Update BPP_queries.sql

--- BPP_queries.sql
+++ BPP_queries.sql
@@ -1,18 +1,19 @@
+--VINCOLI CLIENTE 
+--[
 --UPDATE BPP_Cliente
 --SET [RagioneSociale] = 'N/A'
 --WHERE [TipologiaCliente] = 'Privato';
 
+--select * from BPP_Cliente;
+--select LEN(NumeroTelefono) from BPP_Cliente
 
-select * from BPP_Cliente;
+--ALTER TABLE BPP_Cliente
+--ADD CONSTRAINT CK_CodiceCliente
+--CHECK (LEN(CodiceCliente) = 10);
 
 
-#ALTER TABLE BPP_Cliente
-#ADD CONSTRAINT CK_NumeroTelefono
-#CHECK (DATALENGTH(NumeroTelefono) = 10);
-
-
-#INSERT INTO BPP_Cliente(CodiceCliente,TipologiaCliente,Nome,Cognome,NumeroTelefono,Indirizzo,RagioneSociale,NomeReferente,CognomeReferente)
-#VALUES ( '001967865-8','Privato','Letizia','Palumbo','0922817865','Via Dante','','','')
+--INSERT INTO BPP_Cliente(CodiceCliente,TipologiaCliente,Nome,Cognome,NumeroTelefono,Indirizzo,RagioneSociale,NomeReferente,CognomeReferente)
+--VALUES ( '001967865-8','Privato','Letizia','Palumbo','0922817865','Via Dante','','','')
 
 
 --UPDATE BPP_Cliente
@@ -20,6 +21,46 @@ select * from BPP_Cliente;
 --WHERE TipologiaCliente = 'Privato'
 
 
-#ALTER TABLE BPP_Cliente
-#ADD CONSTRAINT CK_Privato
-#CHECK (TipologiaCliente='Privato' AND (RagioneSociale='N/A' OR RagioneSociale = NULL) AND NomeReferente = NULL AND CognomeReferente = NULL);
+--ALTER TABLE BPP_Cliente
+--ADD CONSTRAINT CK_Privato
+--CHECK (TipologiaCliente='Privato' AND (RagioneSociale='N/A' OR RagioneSociale = NULL) AND NomeReferente = NULL AND CognomeReferente = NULL);
+
+--]
+
+
+
+--VINCOLI OPERATORE
+--[--select LEN(MatricolaOperatore) from BPP_Operatore
+
+--delete from BPP_Operatore
+--select * from BPP_Operatore
+
+--ALTER TABLE BPP_Operatore
+--add constraint CK_MatricolaOperatore
+--check (LEN(MatricolaOperatore) = 10);
+
+
+
+
+--VINCOLI CONTRATTO
+--ALTER TABLE BPP_Contratto
+--add constraint CK_CodiceContratto
+--check (LEN(CodiceContratto) = 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
