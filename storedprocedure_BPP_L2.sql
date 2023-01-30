e9a1f00b399919ffccf77bf3a1d509c88e0f1e64
federicoportogallo <122370565+federicoportogallo@users.noreply.github.com>
2023-01-30 12:10:33 +0100

    Update storedprocedure_BPP_L2.sql

--- storedprocedure_BPP_L2.sql
+++ storedprocedure_BPP_L2.sql
@@ -14,12 +14,13 @@ CREATE PROCEDURE dbo.BPP_L2_createschema_societa_elettrica
 
 AS
 BEGIN
-
-    /*drop table BPP_L2_fact_bolletta 
-	drop table BPP_L2_dim_cliente 
-	drop table BPP_L2_dim_contratto 
-	drop table BPP_L2_dim_contatore 
-	drop table BPP_L2_dim_operatore
+ 
+	/*drop table dbo.BPP_L2_dim_cliente 
+	drop table dbo.BPP_L2_dim_contatore 
+	drop table dbo.BPP_L2_dim_contratto 
+	drop table dbo.BPP_L2_dim_operatore
+	drop table dbo.BPP_L2_dim_data
+	drop table dbo.BPP_L2_fact_bolletta
 	*/
     -- SET NOCOUNT ON added to prevent extra result sets from
     -- interfering with SELECT statements.
@@ -28,7 +29,7 @@ BEGIN
     -- Insert statements for procedure here
 
 
-	create table BPP_L2_dim_cliente(
+	create table dbo.BPP_L2_dim_cliente(
     ChiaveCliente int IDENTITY(0,1) PRIMARY KEY NOT NULL,
     CodiceCliente varchar(20) NOT NULL,
     TipologiaCliente varchar(20),
@@ -42,7 +43,7 @@ BEGIN
 	FlagAttuale char(2)
 	)
 
-	create table BPP_L2_dim_contatore(
+	create table dbo.BPP_L2_dim_contatore(
 	ChiaveContatore int identity(0,1) primary key not null,
 	CodiceContatore char(10) not null,
 	UltimiKWLetti decimal(6,2),
@@ -53,7 +54,7 @@ BEGIN
 	-- KW Letti e Data Lettura contengono sempre la misura più recente
 
 
-	create table BPP_L2_dim_contratto(
+	create table dbo.BPP_L2_dim_contratto(
 	ChiaveContratto int identity(0,1) primary key not null,
 	CodiceContratto char(10),
 	TipologiaContratto varchar(30),
@@ -62,7 +63,7 @@ BEGIN
 	TempoMassimoIntervento int
 	)
 
-	create table BPP_L2_dim_operatore(
+	create table dbo.BPP_L2_dim_operatore(
 	ChiaveOperatore int identity(0,1) primary key not null,
 	MatricolaOperatore char(10) not null, 
 	NomeOperatore varchar(50),
@@ -70,25 +71,25 @@ BEGIN
 	NumeroTelefonoOperatore char(10)
 	)
 
-	/*create table BPP_L2_dim_tempo(
-	[ChiaveTempo] [int] IDENTITY(0,1) NOT NULL,
-	[Data] [date] NULL,
-	[Anno] [char](4) NULL,
-	[Mese] [char](2) NULL,
-	[Giorno] [char](2) NULL,
-	[DescrizioneData] [varchar](30) NULL,
-	[GiornoSettimana] [varchar](20) NULL,
-	[DescrizioneMeseAnno] [varchar](20) NULL,
-	[Weekday/Weekend] [char](7) NULL
+	create table dbo.BPP_L2_dim_data(
+	ChiaveData int IDENTITY(0,1) primary key NOT NULL,
+	[Data] date NULL,
+	Anno char(4) NULL,
+	Mese char(2) NULL,
+	Giorno char(2) NULL,
+	DescrizioneData varchar(30) NULL,
+	GiornoSettimana varchar(20) NULL,
+	DescrizioneMeseAnno varchar(20) NULL,
+	[Weekday/Weekend] char(7) NULL
 	)
-	*/
+	
 
-	create table BPP_L2_fact_bolletta(
-	ChiaveTempo int foreign key references BPP_L2_dim_tempo(ChiaveTempo),
-	ChiaveCliente int foreign key references BPP_L2_dim_cliente(ChiaveCliente),
-	ChiaveContratto int foreign key references BPP_L2_dim_contratto(ChiaveContratto),
-	ChiaveContatore int foreign key references BPP_L2_dim_contatore(ChiaveContatore),
-	ChiaveOperatore int foreign key references BPP_L2_dim_operatore(ChiaveOperatore),
+	create table dbo.BPP_L2_fact_bolletta(
+	ChiaveData int foreign key references dbo.BPP_L2_dim_data(ChiaveData),
+	ChiaveCliente int foreign key references dbo.BPP_L2_dim_cliente(ChiaveCliente),
+	ChiaveContratto int foreign key references dbo.BPP_L2_dim_contratto(ChiaveContratto),
+	ChiaveContatore int foreign key references dbo.BPP_L2_dim_contatore(ChiaveContatore),
+	ChiaveOperatore int foreign key references dbo.BPP_L2_dim_operatore(ChiaveOperatore),
 	DataEmissione date,
 	PeriodoRiferimento varchar(20),
 	DataScadenzaPagamento date,
@@ -101,7 +102,7 @@ BEGIN
 	PrezzoKWhFascia3 decimal(4,3),
 	TotaleDaPagare decimal(8,2),
 	ProfessioneClienteAttuale varchar(50),
-	primary key (ChiaveTempo,ChiaveCliente,ChiaveContratto,ChiaveContatore,ChiaveOperatore)
+	primary key (ChiaveData,ChiaveCliente,ChiaveContratto,ChiaveContatore,ChiaveOperatore)
 	)
 
 
