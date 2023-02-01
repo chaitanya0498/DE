90fc831ac77a85a8cd6b5d56317d9b32b1cef12b
Chaitanya Bhardwaj <chaitanya.0498@gmail.com>
2023-02-01 11:51:34 +0100

    removed old scripts

--- script inserimento dati L1/BPP_Lettura.sql
+++
@@ -1,8 +0,0 @@
-create table BPP_Lettura(
-	CodiceLettura uniqueidentifier not null primary key,
-	CodiceContatore uniqueidentifier FOREIGN KEY REFERENCES BPP_Contatore(CodiceContatore),
-	MatricolaOperatore varchar FOREIGN KEY REFERENCES BPP_Operatore(MatricolaOperatore),
-	KWLetti decimal(5,3),
-	DataOraLettura datetime,
-	)
-	
--- script inserimento dati L1/BPP_Operatore.sql
+++
@@ -1,9 +0,0 @@
-create table BPP_Operatore(
-	MatricolaOperatore varchar not null primary key,
-	Nome varchar,
-	Cognome varchar,
-	NumeroCellulare varchar,
-	)
-
-
-	
